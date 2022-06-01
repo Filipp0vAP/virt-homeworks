8ee05474277edb3adf2508aebeee6fbb496256fa
Aleksandr Filippov <Filipp0vAP@protonmail.com>
2022-06-01 11:19:21 +0300

    add answer for fourth task

--- 05-virt-04-docker-compose/src/terraform/variables.tf
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -13,5 +13,5 @@ variable "yandex_folder_id" {
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd8prj4po9aijhhf3pii"
+  default = "fd8pk0d2hkk4hlil1k89"
 }

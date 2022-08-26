ccca468ca788e07b524142b2a9fbf0f5c1f60fe7
Aleksandr Filippov <Filipp0vAP@protonmail.com>
2022-08-26 23:25:43 +0300

    add answers for all tasks

---
+++ 07-terraform-04-teamwork/terraform/provider.tf
@@ -0,0 +1,16 @@
+# Provider
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+
+provider "yandex" {
+  service_account_key_file = "key.json"
+  cloud_id  = "${var.yc_cloud_id}"
+  folder_id = "${var.yc_folder_id}"
+  zone = "ru-central1-a"
+}

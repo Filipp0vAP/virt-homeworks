525aaefa16a8898e75d90678541580c7ef3c6547
Aleksandr Filippov <38728938+Filipp0vAP@users.noreply.github.com>
2022-08-27 21:35:12 +0300

    Update variables.tf

--- 05-virt-04-docker-compose/src/terraform/variables.tf
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gf79f2bpg42hs52ut3"
+  default = ""
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1g6771tq3c82eglvo0a"
+  default = ""
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd8lbjm1q2aqp6d4he80"
+  default = ""
 }
